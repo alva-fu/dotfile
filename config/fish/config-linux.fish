c1d8b8406720cfbb451003d5e45df5c9b00bb62a
alvaphrodite <109333529+alva-fu@users.noreply.github.com>
2023-03-25 07:56:08 +0800

    Modify pyenv configuration

--- config/fish/config-linux.fish
+++ config/fish/config-linux.fish
@@ -19,4 +19,5 @@ end
 if [ -d "$HOME/.pyenv" ]
   set -gx PYENV_ROOT $HOME/.pyenv
   set -gx PATH $PYENV_ROOT/bin $PATH
+  pyenv init - | source
 end
--- config/fish/config-osx.fish
+++ config/fish/config-osx.fish
@@ -14,4 +14,5 @@ end
 if [ -d "$HOME/.pyenv" ]
   set -gx PYENV_ROOT $HOME/.pyenv
   set -gx PATH $PYENV_ROOT/bin $PATH
+  pyenv init - | source
 end
